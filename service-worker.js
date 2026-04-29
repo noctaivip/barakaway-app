c4d2d3490abdce5be8c62b4a59a98b441c09f842
NOCTAI - Private Capital Systems <noctai.vip@proton.me>
2026-04-29 12:49:42 +0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,45 @@
-self.addEventListener("install", function(event) {
-  console.log("Service Worker installed");
+const CACHE_NAME = 'barakaway-v2';
+
+const ASSETS_TO_CACHE = [
+  '/',
+  '/bottom-nav.js'
+];
+
+// INSTALL
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
 });
 
-self.addEventListener("fetch", function(event) {
+// ACTIVATE (очистка старого кэша)
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// FETCH
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
 });
