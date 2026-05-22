1629e82c3fe5c85924320c876010575cbf423578
NOCTAI - Private Capital Systems <noctai.vip@proton.me>
2026-05-22 10:06:58 +0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    Goal: always prefer the newest files from the network, especially HTML/JS/CSS.
 */
 
-const CACHE_VERSION = '20260517-theme-parity-v192';
+const CACHE_VERSION = '20260517-theme-parity-v193';
 const CACHE_NAME = `barakaway-runtime-${CACHE_VERSION}`;
 
 const APP_SHELL = [
